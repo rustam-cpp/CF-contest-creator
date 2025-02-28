740729cc1170c9aa5b09e53ce8b1849cc709f0aa
rustam-cpp <121849951+rustam-cpp@users.noreply.github.com>
2025-02-28 17:31:45 +0300

    .
    spaces to tabs

--- cf-contest-creator.py
+++ cf-contest-creator.py
@@ -20,9 +20,9 @@ print()
 count = int(input("Enter count of problems: "))
 ratings = list()
 for i in range(count):
-    print("Enter problem", i + 1, "rating range (separated by a space, example: 1500 1700):", end=' ')
-    l, r = map(int, input().split())
-    ratings.append(random.randint(l // 100, r // 100) * 100)
+	print("Enter problem", i + 1, "rating range (separated by a space, example: 1500 1700):", end=' ')
+	l, r = map(int, input().split())
+	ratings.append(random.randint(l // 100, r // 100) * 100)
 
 print("\nNeed to get your already tried problems...")
 
@@ -32,8 +32,8 @@ tried = list()
 print("\nGenerating...")
 
 for s in problems_tried_by_user:
-    p = s.problem
-    tried.append(str(p.contest_id) + p.index)
+	p = s.problem
+	tried.append(str(p.contest_id) + p.index)
 
 # f = open("index.html", "w")
 # f.write("<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n\t<meta charset=\"UTF-8\">\n\t<title>Contest</title>\n</head>\n<body>\n")
@@ -45,18 +45,18 @@ out = list()
 new_problems = input("Do you want to use problems from newer contests? (Y/N): ")
 flag = True
 if new_problems == 'Y':
-    flag = False
+	flag = False
 
 for r in ratings:
-    while True:
-        idx = random.randint(0, size-1)
-        problem = problems[idx]
-        if problem.rating == r and problem.contest_id and tried.count(str(problem.contest_id) + problem.index) == 0 and (flag or problem.contest_id >= 1500) and problem.tags.count("*special") == 0:
-            out.append(": codeforces.com/contest/" + \
-                        str(problem.contest_id) + "/problem/" + problem.index)
-            # f.write("\t<a href=\"" + "https://codeforces.com/contest/" + str(problem.contest_id) + "/problem/" + \
-            #         problem.index + "\" target=\"_blank\">Problem " + str(ix) + "<br></a>\n")
-            break
+	while True:
+		idx = random.randint(0, size-1)
+		problem = problems[idx]
+		if problem.rating == r and problem.contest_id and tried.count(str(problem.contest_id) + problem.index) == 0 and (flag or problem.contest_id >= 1500) and problem.tags.count("*special") == 0:
+			out.append(": codeforces.com/contest/" + \
+						str(problem.contest_id) + "/problem/" + problem.index)
+			# f.write("\t<a href=\"" + "https://codeforces.com/contest/" + str(problem.contest_id) + "/problem/" + \
+			#		 problem.index + "\" target=\"_blank\">Problem " + str(ix) + "<br></a>\n")
+			break
 
 # f.write("</body>\n</html>")
 # f.close()
@@ -65,31 +65,31 @@ print("Contest created!\n")
 
 shuffle = input("Shuffle problems? (Y/N): ")
 if shuffle == 'Y':
-    for i in range(2, count):
-        if random.randint(0, 1) == 1:
-            j = random.randint(0, i - 1)
-            p1 = out[i]
-            p2 = out[j]
-            out[j] = p1
-            out[i] = p2
+	for i in range(2, count):
+		if random.randint(0, 1) == 1:
+			j = random.randint(0, i - 1)
+			p1 = out[i]
+			p2 = out[j]
+			out[j] = p1
+			out[i] = p2
 print("\nGood luck and happy coding!\n")
 print("Contest starts in   ", end='')
 for t in range(before_contest, 0, -1):
-    timer = str(t)
-    if len(timer) == 1:
-        timer = '0' + timer
-    print("\b\b\b", timer, end='')
-    sys.stdout.flush()
-    time.sleep(1)
+	timer = str(t)
+	if len(timer) == 1:
+		timer = '0' + timer
+	print("\b\b\b", timer, end='')
+	sys.stdout.flush()
+	time.sleep(1)
 
 print("\b" * 100, end='')
-print("Contest starts!     \n")
+print("Contest starts!	 \n")
 
 ix = 1
 
 for p in out:
-    print("Problem " + str(ix) + p)
-    ix += 1
+	print("Problem " + str(ix) + p)
+	ix += 1
 
 print()
 
@@ -99,26 +99,26 @@ h = contest_duration // 60
 m = contest_duration % 60
 s = 0
 for i in range(contest_duration * 60):
-    hh = str(h)
-    if len(hh) == 1:
-        hh = '0' + hh
-    mm = str(m)
-    if len(mm) == 1:
-        mm = '0' + mm
-    ss = str(s)
-    if len(ss) == 1:
-        ss = '0' + ss
-    print(hh, mm, ss, sep=':', end='')
-    sys.stdout.flush()
-    time.sleep(1)
-    print("\b" * 100, end='')
-    sys.stdout.flush()
-    s -= 1
-    if s == -1:
-        s = 59
-        m -= 1
-        if m == -1:
-            m = 59
-            h -= 1
+	hh = str(h)
+	if len(hh) == 1:
+		hh = '0' + hh
+	mm = str(m)
+	if len(mm) == 1:
+		mm = '0' + mm
+	ss = str(s)
+	if len(ss) == 1:
+		ss = '0' + ss
+	print(hh, mm, ss, sep=':', end='')
+	sys.stdout.flush()
+	time.sleep(1)
+	print("\b" * 100, end='')
+	sys.stdout.flush()
+	s -= 1
+	if s == -1:
+		s = 59
+		m -= 1
+		if m == -1:
+			m = 59
+			h -= 1
 
 print("The contest is over!")
